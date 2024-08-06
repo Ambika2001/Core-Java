dcf6dad29a4d42f7552a45fe477576078138cbfa
Teja Veera Ganambika Bheesetty <92291051+Ambika2001@users.noreply.github.com>
2024-08-06 15:12:42 +0530

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,13 @@
+//Write a aprogram to perform sum of two numbers without using Third Variable? 
+import java.util.Scanner;
+class  Example1 {
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Number:");
+		int a = sc.nextInt();
+		System.out.println("Enter Second Number:");
+		int b = sc.nextInt();
+		System.out.println("Sum of Two Numbers is = "+(a+b));
+	}
+}
